690da993383da9b7d82af3b8b59a86a59de28bc0
Dante Vilches <dante.vilchesc@gmail.com>
2025-11-26 12:27:22 -0300

    manifest v3

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  // Opciones básicas de configuración para PWA
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // Deshabilita PWA en desarrollo para que no cachee mientras programas
-})
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -16,5 +11,9 @@ const nextConfig = {
 }
 
 
-
-module.exports = withPWA(nextConfig)
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
