8a2ca864bd5b9dd666cb1ed4a62a1d603cf95260
Dante Vilches <dante.vilchesc@gmail.com>
2025-11-26 12:39:38 -0300

    manifest

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -11,9 +9,3 @@ const nextConfig = {
 }
 
 
-export default withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})(nextConfig);
